2891e16c519cd73b6d77e7a71ffd86c01304d2ef
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2025-01-08 23:12:57 +0300

    currency list update

--- main.py
+++ main.py
@@ -65,14 +65,14 @@ WEBSOCKET_CURRENCY_PAIRS = {
     "UNI/USD": "UNI",
 
     # Forex pairs
-    "EUR/USD": "EUR/USD",
-    "GBP/USD": "GBP/USD",
-    "USD/JPY": "USD/JPY",
-    "USD/CHF": "USD/CHF",
-    "AUD/USD": "AUD/USD",
-    "USD/CAD": "USD/CAD",
-    "EUR/GBP": "EUR/GBP",
-    "EUR/JPY": "EUR/JPY",
+    # "EUR/USD": "EUR/USD",
+    # "GBP/USD": "GBP/USD",
+    # "USD/JPY": "USD/JPY",
+    # "USD/CHF": "USD/CHF",
+    # "AUD/USD": "AUD/USD",
+    # "USD/CAD": "USD/CAD",
+    # "EUR/GBP": "EUR/GBP",
+    # "EUR/JPY": "EUR/JPY",
 
     # Additional currencies
     "USD/KSH": "KES",
